04aa3f590269e5777efeca7b8c39aa246fc1ac29
Peter Michaleczky <michaleczky@hotmail.com>
2016-03-07 15:11:53 +0100

    DataCollector first working copy

--- WeatherDataCollector/WeatherDataCollector.py
+++ WeatherDataCollector/WeatherDataCollector.py
@@ -1,11 +1,10 @@
 import uuid
 import json
 import inspect
-
-# Data Ingestion endpoint in the PX Cloud
-DI_ENDPOINT = "wss://gateway-predix-data-services.run.aws-usw02-pr.ice.predix.io/v1/stream/messages"
-
-PREDIX_ZONE_ID = "565348bc-eda6-4a7b-b72d-e272351e06a1"
+import websocket
+import time, thread
+import logging, sys
+import settings
 
 class ObjectEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -53,7 +52,45 @@ class TS_Body:
         self.attributes = attributes if attributes else {}
 
 
-payload = TS_Payload()
-payload.add_tag('TEMPERATURE', [ [1, 12, 3], [2, 15, 3], [3, 13, 3] ], {"location" : "Budapest"})
-payload.add_tag('PRESSURE', [ [1, 980, 3], [2, 1010, 3], [3, 1030, 3] ], {"location" : "Budapest"})
-print json.dumps(payload, cls=ObjectEncoder)
+# setup the logging    
+logging.basicConfig(format = settings.LOG_FORMAT, stream = sys.stdout, level = settings.LOG_LEVEL)
+
+
+def job(ws):
+    payload = TS_Payload()
+    payload.add_tag('TEMPERATURE', [ [1, 12, 3], [2, 15, 3], [3, 13, 3] ], {"location" : "Budapest"})
+    payload.add_tag('PRESSURE', [ [1, 980, 3], [2, 1010, 3], [3, 1030, 3] ], {"location" : "Budapest"})
+    payload_json = json.dumps(payload, cls=ObjectEncoder)    
+    ws.send(payload_json)
+
+def ws_on_message(ws, message):
+    logging.info(message)
+
+def ws_on_error(ws, error):
+    logging.error, error
+
+def ws_on_close(ws):
+    logging.info("WebSocket closed")
+
+def ws_on_open(ws):
+    
+    def run(*args):
+        for i in range(3):
+            time.sleep(1)
+            job(ws)
+        time.sleep(1)
+        ws.close()
+    
+    logging.info("WebSocket to %s opened", ws.url)
+    thread.start_new_thread(run, ())
+
+if __name__ == "__main__":
+    #ws_header = "Authorization: Bearer <%s>\nPredix-Zone-Id: <%s>Origin: %s\n" % (settings.BEARER_TOKEN, settings.PREDIX_ZONE_ID, settings.ORIGIN)
+    ws_header = [
+        "Authorization: Bearer %s" % settings.BEARER_TOKEN,
+        "Predix-Zone-Id: %s" % settings.PREDIX_ZONE_ID,
+        #"Origin: %s" % settings.ORIGIN
+    ]
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(settings.DI_ENDPOINT, on_message = ws_on_message, on_error = ws_on_error, on_close = ws_on_close, on_open = ws_on_open, header = ws_header)
+    ws.run_forever()
